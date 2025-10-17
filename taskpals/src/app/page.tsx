945c091fac00b22a63574f92b4946b406a1affe7
Nelly <kchen0116@gmail.com>
2025-10-17 15:27:25 +1100

    Updates

--- taskpals/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import TaskPals from '../components/TaskPals';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-100 py-8">
-      <TaskPals />
-    </main>
-  );
-}
